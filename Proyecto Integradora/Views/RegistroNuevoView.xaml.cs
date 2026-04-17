de56954230f70825ec4e626455e45f4bcfc02c35
JaretEduardo <jaret.gonzalez.ct59@gmail.com>
2026-04-16 22:43:27 -0600

    fix: hash password and see

--- Proyecto Integradora/Views/RegistroNuevoView.xaml.cs
+++ Proyecto Integradora/Views/RegistroNuevoView.xaml.cs
@@ -19,6 +19,9 @@ namespace Proyecto_Integradora.Views
 {
     public partial class RegistroNuevoView : Page
     {
+        private bool _isSyncingPassword;
+        private bool _isPasswordVisible;
+
         public RegistroNuevoView()
         {
             InitializeComponent();
@@ -26,11 +29,61 @@ namespace Proyecto_Integradora.Views
 
         private void txtPassword_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
         {
+            if (_isSyncingPassword)
+            {
+                return;
+            }
+
+            _isSyncingPassword = true;
+            txtPasswordVisible.Text = txtPassword.Password;
+
             // Accedemos al ViewModel y actualizamos la propiedad manualmente
             if (this.DataContext is RegistroViewModel viewModel)
             {
-                viewModel.Contrasenia = ((PasswordBox)sender).Password;
+                viewModel.Contrasenia = txtPassword.Password;
             }
+
+            _isSyncingPassword = false;
+        }
+
+        private void txtPasswordVisible_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_isSyncingPassword || !_isPasswordVisible)
+            {
+                return;
+            }
+
+            _isSyncingPassword = true;
+            txtPassword.Password = txtPasswordVisible.Text;
+
+            if (this.DataContext is RegistroViewModel viewModel)
+            {
+                viewModel.Contrasenia = txtPasswordVisible.Text;
+            }
+
+            _isSyncingPassword = false;
+        }
+
+        private void TogglePasswordVisibility_Click(object sender, RoutedEventArgs e)
+        {
+            _isPasswordVisible = !_isPasswordVisible;
+
+            if (_isPasswordVisible)
+            {
+                txtPasswordVisible.Text = txtPassword.Password;
+                txtPassword.Visibility = Visibility.Collapsed;
+                txtPasswordVisible.Visibility = Visibility.Visible;
+                btnTogglePassword.Content = "Ocultar";
+                txtPasswordVisible.Focus();
+                txtPasswordVisible.CaretIndex = txtPasswordVisible.Text.Length;
+                return;
+            }
+
+            txtPassword.Password = txtPasswordVisible.Text;
+            txtPasswordVisible.Visibility = Visibility.Collapsed;
+            txtPassword.Visibility = Visibility.Visible;
+            btnTogglePassword.Content = "Mostrar";
+            txtPassword.Focus();
         }
     }
 }
